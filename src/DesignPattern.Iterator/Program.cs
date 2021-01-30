05fc7cc57393d6885c14afbb04ed2812c90ec3ae
jinjupeng <im.jp@outlook.com>
2021-01-30 21:34:15 +0800

    迭代器模式

---
+++ src/DesignPattern.Iterator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace DesignPattern.Iterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 迭代器模式：Array 和 List 各自实现了自己的迭代器（通过继承接口IEnumerator）
+             * 迭代器模式是一种行为设计模式， 让你能在不暴露集合底层表现形式 （列表、 栈和树等） 的情况下遍历集合中所有的元素。
+             * 
+             */
+            // The client code may or may not know about the Concrete Iterator
+            // or Collection classes, depending on the level of indirection you
+            // want to keep in your program.
+            var collection = new WordsCollection();
+            collection.AddItem("First");
+            collection.AddItem("Second");
+            collection.AddItem("Third");
+
+            Console.WriteLine("Straight traversal:");
+
+            foreach (var element in collection)
+            {
+                Console.WriteLine(element);
+            }
+
+            Console.WriteLine("\nReverse traversal:");
+
+            collection.ReverseDirection();
+
+            foreach (var element in collection)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
